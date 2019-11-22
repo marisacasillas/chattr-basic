7a7cae398eb944be814ef4c770a100ff877c012b
Marisa Casillas <middycasillas@gmail.com>
2019-11-22 17:07:38 +0100

    add elan-basic-txt table type

--- tabularize-data.R
+++ tabularize-data.R
@@ -1,32 +1,36 @@
 library(tidyverse)
 
-aastbl.classes <- paste0(c("character", "character",
-                    "integer", "integer", "integer", "character"), collapse = "")
-aastbl.colnames <- c("tier", "speaker", "start.ms", "stop.ms", "duration", "value")
+ebtbl.colnames <- c("tier", "speaker", "start.ms", "stop.ms", "duration", "value")
 
 # This must be in the rigid spchtbl format specified in the docs
-read_spchtbl <- function(filepath, tbltype) {
-  if (tbltype != "aas-elan-txt") {
+read_spchtbl <- function(filepath, tbltype, cliptier) {
+  if (tbltype == "aas-elan-txt") {
+    spchtbl <- aas_to_spchtbl(filepath, cliptier)
+  } else if (tbltype == "elan-basic-txt") {
+    spchtbl <- elanbasic_to_spchtbl(filepath, cliptier)
+  } else {
     print("Sorry, that file type isn't available!")
-    next()
   }
-  spchtbl <- aas_to_spchtbl(filepath)
   return(spchtbl)
 }
 
-aas_to_spchtbl <- function(tbl) {
-  aastbl <- read_delim(file = tbl, delim = "\t", col_names = FALSE)
-  # check that the column types are correct
-  col.types <- paste0(unlist(lapply(aastbl, class)), collapse = "")
-  # add the standard column names
-  names(aastbl) <- aastbl.colnames
-  # start with just the utterances
+aas_to_spchtbl <- function(tbl, cliptier) {
+  aastbl <- read_delim(file = tbl, delim = "\t",
+    col_names = ebtbl.colnames, col_types = cols(
+      tier = col_character(),
+      speaker = col_character(),
+      start.ms = col_integer(),
+      stop.ms = col_integer(),
+      duration = col_integer(),
+      value = col_character()
+    ))
+  # extract the top-level utterance tiers
   wide.aastbl <- filter(aastbl, tier == speaker)
-  # add in addressee information
+  # add in addressee information for each utterance
   xds.aastbl <- filter(aastbl, grepl('xds', tier)) %>%
     select(speaker, start.ms, value) %>%
     rename(addressee = value)
-  # add in vocal maturity data
+  # now add in vocal maturity data (to-do)
 #  vcm.aastbl <- filter(aastbl, speaker == "CHI" & speaker != tier) %>%
 #    spread(tier, value) %>%
 #    rename(vcm = 'vcm@CHI', lex = 'lex@CHI', mwu = 'mwu@CHI') %>%
@@ -35,5 +39,40 @@ aas_to_spchtbl <- function(tbl) {
   wide.aastbl <- left_join(wide.aastbl, xds.aastbl) %>%
 #    left_join(vcm.aastbl) %>%
     select(speaker, start.ms, stop.ms, addressee)
-  return(wide.aastbl)
+  # add in information about the annotated regions
+  # (if no annotation, stop and tell the user)
+  if (cliptier %in% unique(aastbl$tier)) {
+    clip.tbl <- filter(aastbl, tier == cliptier) %>%
+      mutate(speaker = paste0("annotated-", start.ms, "-", value),
+        addressee = NA) %>%
+      select(speaker, start.ms, stop.ms, addressee)
+    wide.aastbl <- bind_rows(clip.tbl, wide.aastbl)
+    return(wide.aastbl)
+  } else {
+    print("Error: no rows from the clip tier found.")
+  }
+}
+
+elanbasic_to_spchtbl <- function(tbl, cliptier) {
+  ebtbl <- read_delim(file = tbl, delim = "\t",
+    col_names = ebtbl.colnames, col_types = cols(
+      tier = col_character(),
+      speaker = col_character(),
+      start.ms = col_integer(),
+      stop.ms = col_integer(),
+      duration = col_integer(),
+      value = col_character()
+    )) %>%
+    select(tier, start.ms, stop.ms, value) %>%
+    rename(speaker = tier)
+  # add in information about the annotated regions
+  # (if no annotation, stop and tell the user)
+  if (cliptier %in% unique(ebtbl$speaker)) {
+    clip.tbl <- filter(ebtbl, speaker == cliptier) %>%
+      mutate(speaker = paste0("annotated-", start.ms, "-", value))
+    ebtbl <- bind_rows(clip.tbl, ebtbl)
+    return(ebtbl)
+  } else {
+    print("Error: no rows from the clip tier found.")
+  }
 }
--- testpage.R
+++ testpage.R
@@ -2,5 +2,10 @@ source("tabularize-data.R")
 source("transition-detectors.R")
 
 my.elan.txt <- "test_files/AAS-tabular/VanFJ11-0GS0.txt"
+ai.elan.txt <- "test_files/AltELAN-tabular/CT_sample1.txt"
 
-data <- read_spchtbl(filepath = my.elan.txt, tbltype = "aas-elan-txt")
+my.data <- read_spchtbl(filepath = my.elan.txt,
+  tbltype = "aas-elan-txt", cliptier = "code")
+ai.data <- read_spchtbl(filepath = ai.elan.txt,
+  tbltype = "elan-basic-txt", cliptier = "Coded Segment")
+
