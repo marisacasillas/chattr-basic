0a27914681ecae5bb878f002b3de486a9ad2c097
marisacasillas <middycasillas@gmail.com>
2020-07-22 19:25:20 +0200

    notes on baseline

---
+++ estimate-baseline.R
@@ -0,0 +1,17 @@
+
+shuffle_vocs <- function(tbl) {
+  
+}
+
+fetch_baseline <- function(tbl, n.runs = 100,
+                           spchtbl, allowed.gap, allowed.overlap,
+                           min.utt.dur, focus.child, interactants,
+                           addressee.tags, mode) {
+  # for runs
+  # shuffle vocs
+  # fetch_transitions
+  # fetch_intseqs
+  # return tables for both
+  
+  # option for just shuffling focal or just shuffling interactants?
+}
