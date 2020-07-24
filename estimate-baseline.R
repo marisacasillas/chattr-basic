d6400b97fc860d569f3d750125a2575af0c904a6
marisacasillas <middycasillas@gmail.com>
2020-07-24 14:34:49 +0200

    add draft of shuffle function
    it's too slow

--- estimate-baseline.R
+++ estimate-baseline.R
@@ -1,14 +1,56 @@
+ann.marker <- "annotated-" ## ALSO USED IN TABULARIZE DATA
+start.ann <- paste0("^", ann.marker, "\\d+", collapse = "") ## ALSO USED IN CHATTR HELPERS
+modes <- c("strict", "stretch", "luqr", "qulr")
+prewindow.string <- "pre"
+postwindow.string <- "post"
 
 shuffle_vocs <- function(tbl) {
-  
-  # split n (total non-spch time) into k (# utts + 1) parts of random size
-  # (NOTE: must be in ms; uses integers!)
-  non.spch.dur <- TBD
-  num.between.utt.intervals <- TBD
-  between.utt.durs <- sample(as.vector(sample(
-    1:num.between.utt.intervals, non.spch.dur, replace = TRUE)))
-  # if there are fewer than the max number of between.utt.intervals, set
-  # these to zero and re-randomize
+  unique.spkrs <- unique(tbl$speaker)[which((!grepl(
+    ann.marker, unique(tbl$speaker))))]
+  unique.clips <- unique(tbl$speaker)[which((grepl(
+    ann.marker, unique(tbl$speaker))))]
+  shuffled.tbl <- tbl %>%
+    filter(grepl(ann.marker, speaker))
+  for (spkr in unique.spkrs) {
+    for (clip in unique.clips) {
+      clip.idx <- which(tbl$speaker == clip)
+      clip.on <- tbl$start.ms[clip.idx]
+      clip.off <- tbl$stop.ms[clip.idx]
+      clip.dur <- tbl$duration[clip.idx]
+      spkr.vocs <- tbl %>%
+        filter(speaker == spkr &
+                 start.ms >= clip.on & stop.ms <= clip.off)
+      if (nrow(spkr.vocs) > 0) {
+        # split n (total non-spch time) into k (# utts + 1) parts of random size
+        # (NOTE: must be in ms, uses integers)
+        # very slow :(
+        non.spch.dur <- clip.dur - sum(spkr.vocs$duration)
+        num.between.utt.intervals <- nrow(spkr.vocs) + 1
+        between.utt.durs <- as.vector(table(sample(
+          1:num.between.utt.intervals, non.spch.dur, replace = TRUE)))
+        # If there are fewer than the max number of between.utt.intervals, set
+        # these to zero and re-randomize
+        if (length(between.utt.durs) < num.between.utt.intervals) {
+          missing.durs <- num.between.utt.intervals - length(between.utt.durs)
+          between.utt.durs <- sample(c(between.utt.durs, rep(0, missing.durs)))
+        }
+        # Create shuffled speaker onsets
+        ## randomize utterance order
+        spkr.vocs <- slice(spkr.vocs, sample(1:n()))
+        ## insert randomized between-utterance periods
+        spkr.vocs$onset.interval <- between.utt.durs[
+          1:length(between.utt.durs) - 1]
+        spkr.vocs$dur.cum <- cumsum(spkr.vocs$duration)
+        spkr.vocs$interval.cum <- cumsum(spkr.vocs$onset.interval)
+        spkr.vocs$stop.ms <- spkr.vocs$interval.cum + spkr.vocs$dur.cum
+        spkr.vocs$start.ms <- spkr.vocs$stop.ms - spkr.vocs$duration
+        spkr.vocs <- spkr.vocs %>%
+          dplyr::select(-onset.interval, -dur.cum, - interval.cum)
+        # Write shuffled onsets to the output table
+        shuffled.tbl <- bind_rows(shuffled.tbl, spkr.vocs)
+      }
+    }
+  }
 }
 
 # option for just shuffling focal or just shuffling interactants?
